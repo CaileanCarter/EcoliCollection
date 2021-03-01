efcc4d3793231cf0f38229890245056bdaf9835e
CaileanCarter <cailean.carter@quadram.ac.uk>
2021-03-01 15:58:53 +0000

    first commit

---
+++ fetch_Entrez_metadata.py
@@ -0,0 +1,79 @@
+from Bio import Entrez
+import numpy as np
+import pandas as pd
+
+
+def fetch_id(term):
+    handle = Entrez.esearch(db="assembly", term=term)
+    record = Entrez.read(handle)
+    idlist = record["IdList"]
+
+    if len(idlist) > 1:
+        print(f"{term} returned more than 1 result")
+    return idlist[0]
+
+def fetch_summary(id_num):
+    esummary_handle = Entrez.esummary(db="assembly", id=id_num, report="full")
+    esummary_record = Entrez.read(esummary_handle, validate=False)
+    summary = esummary_record['DocumentSummarySet']['DocumentSummary'][0]
+
+    return summary
+
+
+def fetch_parts(summary):
+    AssemblyAccession = summary["AssemblyAccession"]
+    AssemblyStatus = summary["AssemblyStatus"]
+    WGS = summary["WGS"]
+    Coverage = summary["Coverage"]
+    SubmissionDate = summary["SubmissionDate"]
+    LastUpdateDate = summary["LastUpdateDate"]
+
+    return AssemblyAccession, AssemblyStatus, WGS, Coverage, SubmissionDate, LastUpdateDate
+
+
+def fetch_all(term):
+    id_num = fetch_id(term)
+    summary = fetch_summary(id_num)
+    return fetch_parts(summary)
+
+
+def fetch_sequence():
+    for index in ena[ena["FASTA"].isna()].index:
+        id_num = fetch_id(index)
+        summary = fetch_summary(id_num)
+        ftp = summary["FtpPath_RefSeq"]
+        
+
+        unzipped = path.join(DIR, f"{index}.fasta")
+        zipped = unzipped + ".gz"
+
+        label = path.basename(ftp)
+        if not ftp:
+            continue
+        link = ftp +"/"+ label+'_genomic.fna.gz'
+        print(link)
+
+        ena.at[index, "FASTA"] = link
+
+        get_FASTA(url=link, fp=zipped)
+        unzip_gz(zipped, unzipped)
+
+
+
+def main(ena):
+    dump = {"AssemblyAccession" : [],
+        "AssemblyStatus" : [],
+        "WGS" : [],
+        "Coverage" : [],
+        "SubmissionDate" : [],
+        "LastUpdateDate" : []}
+
+    for index in ena.index:
+        results = fetch_all(index)
+        for key, result in zip(dump.keys(), results):
+            dump[key].append(result if result else np.NaN)
+        
+    trial = pd.DataFrame(dump, index=ena.index)
+
+    return trial
+
---
+++ utils.py
@@ -0,0 +1,33 @@
+import numpy as np
+from os import path
+
+def phylo(x):
+    try:
+        p = open(path.join(phylogroups, f"{x}.txt")).readlines()[-1]
+        result = p.split()[1]
+        return result
+    except:
+        return np.NaN
+
+ena["phylogroup"] = ena.index.map(phylo)
+
+
+
+def EzClerment(fp, output, script="EzClermont.py"):
+    OUT = path.basename(fp).replace(".fasta", ".txt")
+    system(f'python "{script}" "{fp}" 1> "{output}{OUT}" 2>&1')
+
+
+def ectype(x):
+    PATH = os.path.join(DIR, f"{x}.tabular")
+    try:
+        result = open(PATH).readlines()[-1].split()
+    except:
+        return np.NaN
+    
+    Otype = result[1]
+    Htype = result[2]
+
+    response = " ".join([Otype, Htype])
+
+    return response
